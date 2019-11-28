631e52011bf70f2d7735dac2f71c3ddd7d35dac4
Aashirwad Sinha <aashirwad@ratibpal.com>
2019-11-28 21:01:38 +0530

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Aashirwad Sinha on 11/28/19.
+//  Copyright © 2019 Credit Suisse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
